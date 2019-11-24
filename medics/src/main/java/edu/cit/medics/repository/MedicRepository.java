d9896dacbf145e266211bd86f93eff2c200c21a1
wosi1018 <wosi1018@hs-karlsruhe.de>
2019-11-24 13:30:51 +0000

    deleted the two deprecated services

--- medics/src/main/java/edu/cit/medics/repository/MedicRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.cit.medics.repository;
-
-import edu.cit.medics.model.Medic;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MedicRepository extends MongoRepository<Medic, String> {
-
-    Medic findByEmail(String eMail);
-}
