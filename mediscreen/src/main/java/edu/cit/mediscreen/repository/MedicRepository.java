7dca818f85bf854ec89396d4ed84c7fadfc3e2cc
Mantas44 <57907923+Mantas44@users.noreply.github.com>
2019-11-24 14:20:51 +0000

    Added Patient App

--- mediscreen/src/main/java/edu/cit/mediscreen/repository/MedicRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.cit.mediscreen.repository;
-
-import edu.cit.mediscreen.model.Medic;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MedicRepository extends MongoRepository<Medic, String> {
-
-    Medic findByEmail(String email);
-}
